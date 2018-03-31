258ec97c51643e957850c948f5574a7fcaff8bdd
Cook Green <cookgreen@users.noreply.github.com>
2018-03-31 21:18:45 +0800

    Remove useless Install section

---
+++ yr/Warheads/VirusWarhead.cs
@@ -0,0 +1,20 @@
+﻿using OpenRA.Mods.Common.Warheads;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenRA;
+using OpenRA.Traits;
+
+namespace yr.Warheads
+{
+    class VirusWarhead : Warhead
+    {
+        public override void DoImpact(Target target, Actor firedBy, IEnumerable<int> damageModifiers)
+        {
+            World world = firedBy.World;
+            
+        }
+    }
+}
