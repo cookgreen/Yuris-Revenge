d2d231aa7911730214e8823c9d80ccece2c6b59a
Cook Green <cookgreen@users.noreply.github.com>
2018-10-18 08:12:46 +0800

    Fix #1

--- setdir-yr.cmd
+++ setdir-yr.cmd
@@ -1,3 +1,8 @@
-mkdir %cd%\mods\ra2
+mkdir "%cd%\mods\ra2"
 
-xcopy "%cd%\ra2\mods\ra2\OpenRA.Mods.RA2.dll" "%cd%/mods/ra2/OpenRA.Mods.RA2.dll" /V /Y
+xcopy "%cd%\ra2\mods\ra2\OpenRA.Mods.RA2.dll" "%cd%\mods\ra2\OpenRA.Mods.RA2.dll" /V /Y
+
+mkdir "%cd%\mods\common"
+
+xcopy "%cd%\engine\mods\common\OpenRA.Mods.Cnc.dll" "%cd%\mods\common\OpenRA.Mods.Cnc.dll" /V /Y
+xcopy "%cd%\engine\mods\common\OpenRA.Mods.Common.dll" "%cd%\mods\common\OpenRA.Mods.Common.dll" /V /Y
