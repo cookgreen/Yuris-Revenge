3bd26b2e2043e6e9fd05c3c076d66c57d703edbe
Cook Green <dell20091234@gmail.com>
2018-10-26 14:13:07 +0800

    CrewKillable Vehicle

--- OpenRA.Mods.YR/Traits/CrewKillable.cs
+++ OpenRA.Mods.YR/Traits/CrewKillable.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OpenRA.Mods.Common.Traits;
+using OpenRA.Traits;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,7 +11,18 @@ namespace OpenRA.Mods.YR.Traits
     /// <summary>
     /// Change victim owner to Netural
     /// </summary>
-    class CrewKillable
+    public class CrewKillableInfo : PausableConditionalTraitInfo
     {
+        public override object Create(ActorInitializer init)
+        {
+            return new CrewKillable(this);
+        }
+    }
+
+    public class CrewKillable : PausableConditionalTrait<CrewKillableInfo>
+    {
+        public CrewKillable(CrewKillableInfo info) : base(info)
+        {
+        }
     }
 }
---
+++ OpenRA.Mods.YR/Warheads/KillCrewWarhead.cs
@@ -0,0 +1,47 @@
+﻿using OpenRA.Mods.Common.Warheads;
+using OpenRA.Mods.YR.Traits;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenRA.Mods.YR.Warheads
+{
+    public class KillCrewWarhead : SpreadDamageWarhead
+    {
+        public override void DoImpact(WPos pos, Actor firedBy, IEnumerable<int> damageModifiers)
+        {
+            base.DoImpact(pos, firedBy, damageModifiers);
+
+            World w = firedBy.World;
+
+            Player neutralPlayer = null;
+
+            Player[] players = w.Players;
+            for (int i = 0; i < players.Length; i++)
+            {
+                if (players[i].InternalName == "Neutral")
+                {
+                    neutralPlayer = players[i];
+                    break;
+                }
+            }
+
+            var victimActors = w.FindActorsInCircle(pos, new WDist(1));
+            foreach (Actor victim in victimActors)
+            {
+                if (victim.TraitsImplementing<CrewKillable>().Count() > 0)//This actor can be crew killed
+                {
+                    if (neutralPlayer != null)
+                    {
+                        victim.ChangeOwner(neutralPlayer);
+                    }
+                }
+            }
+        }
+        public override void DoImpact(Actor victim, Actor firedBy, IEnumerable<int> damageModifiers)
+        {
+        }
+    }
+}
