1000d692c55e33955bd059ec7d4bb7c9db73ed29
Cook Green <cookgreen@users.noreply.github.com>
2018-10-26 10:56:52 +0800

    fixup super weapon sound

--- setdir-yr.cmd
+++ setdir-yr.cmd
@@ -4,8 +4,8 @@ xcopy "%cd%\ra2\mods\ra2\*" "%cd%\mods\ra2\" /V /Y /S /H
 
 mkdir "%cd%\mods\common"
 
-xcopy "%cd%\engine\mods\common\OpenRA.Mods.Cnc.dll" "%cd%\mods\common\OpenRA.Mods.Cnc.dll" /V /Y
-xcopy "%cd%\engine\mods\common\OpenRA.Mods.Common.dll" "%cd%\mods\common\OpenRA.Mods.Common.dll" /V /Y
+xcopy "%cd%\engine\mods\common\OpenRA.Mods.Cnc.dll" "%cd%\mods\common\" /V /Y
+xcopy "%cd%\engine\mods\common\OpenRA.Mods.Common.dll" "%cd%\mods\common\l" /V /Y
 
 mkdir "%cd%\mods\modcontent"
 
