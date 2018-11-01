594db24d9788064d221116b10d749864dfb2cc1c
Cook Green <dell20091234@gmail.com>
2018-11-01 21:21:40 +0800

    Machine Shop

---
+++ OpenRA.Mods.YR/Traits/GrantConditionOnCapture.cs
@@ -0,0 +1,108 @@
+﻿using OpenRA.Mods.Common;
+using OpenRA.Mods.Common.Traits;
+using OpenRA.Traits;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenRA.Mods.YR.Traits
+{
+    public class GrantConditionOnCaptureInfo : ConditionalTraitInfo
+    {
+        [FieldLoader.Require]
+        [GrantedConditionReference]
+        [Desc("Grant a condition when a faction capture this actor")]
+        public readonly string GrantCaptureCondition;
+        public override object Create(ActorInitializer init)
+        {
+            return new GrantConditionOnCapture(init, this);
+        }
+    }
+    public class GrantConditionOnCapture : ConditionalTrait<GrantConditionOnCaptureInfo>, INotifyOwnerChanged, ITick
+    {
+        private int conditionToken;
+        ConditionManager conditionManager;
+        private GrantConditionOnCaptureInfo info;
+        private Player thisPlayer;
+        private Player oldPlayer;
+        private string thisFactionName;
+
+        public GrantConditionOnCapture(ActorInitializer init, GrantConditionOnCaptureInfo info) : base(info)
+        {
+            this.info = info;
+            thisFactionName = init.Contains<FactionInit>() ? init.Get<FactionInit, string>() : init.Self.Owner.Faction.InternalName;
+        }
+
+
+        protected override void Created(Actor self)
+        {
+            conditionManager = self.Trait<ConditionManager>();
+
+            base.Created(self);
+        }
+
+        public void OnOwnerChanged(Actor self, Player oldOwner, Player newOwner)
+        {
+            if(newOwner.InternalName == "Netural")
+            {
+                return;
+            }
+            World w = self.World;
+            if (thisPlayer != newOwner)
+            {
+                thisPlayer = newOwner;
+                oldPlayer = oldOwner;
+
+                TraitDisabled(self);
+                TraitEnabled(self);
+                
+            }
+        }
+        protected override void TraitEnabled(Actor self)
+        {
+            if (conditionToken == ConditionManager.InvalidConditionToken)
+            {
+                //Grant condition to all actors belong to this faction
+                World w = self.World;
+                var actorsBelongToThisFaction = w.Actors.Where(o => o.Owner == thisPlayer);
+                foreach (var actor in actorsBelongToThisFaction)
+                {
+                    conditionToken = conditionManager.GrantCondition(actor, info.GrantCaptureCondition);
+                }
+            }
+        }
+
+        protected override void TraitDisabled(Actor self)
+        {
+            if (conditionToken == ConditionManager.InvalidConditionToken)
+                return;
+
+            //Disable condition to all actors belong to old faction
+            World w = self.World;
+            var actorsBelongToOldFaction = w.Actors.Where(o => o.Owner == oldPlayer);
+            foreach (var actor in actorsBelongToOldFaction)
+            {
+                conditionToken = conditionManager.RevokeCondition(actor, conditionToken);
+            }
+        }
+
+        public void Tick(Actor self)
+        {
+            if(self.IsDead)
+            {
+                if (conditionToken == ConditionManager.InvalidConditionToken)
+                    return;
+
+                //Disable condition to all actors belong to this faction when the actor is dead
+                World w = self.World;
+                var actors = w.Actors.Where(o => o.Owner == thisPlayer);
+                foreach (var actor in actors)
+                {
+                    conditionToken = conditionManager.RevokeCondition(actor, conditionToken);
+                }
+            }
+        }
+    }
+}
