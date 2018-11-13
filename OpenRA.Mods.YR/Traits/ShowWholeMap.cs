3671b965e069c43fa23bc6667a9e77074ef5f3c7
Cook Green <dell20091234@gmail.com>
2018-11-13 14:14:49 +0800

    Allied Satellite Link can show whole map

---
+++ OpenRA.Mods.YR/Traits/ShowWholeMap.cs
@@ -0,0 +1,121 @@
+﻿using OpenRA.Mods.Common.Traits;
+using OpenRA.Traits;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenRA.Mods.YR.Traits
+{
+    public class ShowWholeMapInfo : ConditionalTraitInfo
+    {
+        public override object Create(ActorInitializer init)
+        {
+            return new ShowWholeMap(init, this);
+        }
+    }
+    public class ShowWholeMap : ConditionalTrait<ShowWholeMapInfo>, INotifyOwnerChanged, ITick, INotifySold, INotifyKilled
+    {
+        private PowerManager powerMgr;
+        private ShowWholeMapInfo info;
+        private Actor self;
+        private bool disableShround;
+        public ShowWholeMap(ActorInitializer init, ShowWholeMapInfo info) : base(info)
+        {
+            self = init.Self;
+            this.info = info;
+            powerMgr = self.Owner.PlayerActor.Trait<PowerManager>();
+        }
+
+        public void OnOwnerChanged(Actor self, Player oldOwner, Player newOwner)
+        {
+            if(newOwner.InternalName=="Netural")
+            {
+                return;
+            }
+
+            if(self.Owner == oldOwner)
+            {
+                disableShround = true;
+            }
+            else if(self.Owner == newOwner)
+            {
+                disableShround = false;
+            }
+
+            powerMgr = newOwner.PlayerActor.Trait<PowerManager>();
+
+            oldOwner.Shroud.Disabled = false;
+            newOwner.Shroud.Disabled = true;
+        }
+
+        public void Tick(Actor self)
+        {
+            if(powerMgr.PowerState == PowerState.Low)
+            {
+                //low power
+                disableShround = false;
+            }
+            else
+            {
+                disableShround = true;
+            }
+
+            if (this.self.IsDead)
+            {
+                disableShround = false;
+            }
+            UpdateShroundState(disableShround);
+        }
+
+        protected override void Created(Actor self)
+        {
+            disableShround = true;
+            UpdateShroundState(disableShround);
+
+            base.Created(self);
+        }
+
+        public void Selling(Actor self)
+        {
+        }
+
+        public void Sold(Actor self)
+        {
+            disableShround = false;
+            UpdateShroundState(disableShround);
+        }
+
+        public void Killed(Actor self, AttackInfo e)
+        {
+            var allActorsCanShowWholeMap = this.self.Owner.World.Actors.Where(o => o.TraitsImplementing<ShowWholeMap>().Count() > 0);
+            if (allActorsCanShowWholeMap.Count() > 0)
+            {
+                //We still have other actors that can show whole map, let's check power
+                if (powerMgr.PowerState == PowerState.Low)
+                {
+                    //low power
+                    disableShround = false;
+                }
+                else
+                {
+                    disableShround = true;
+                }
+                UpdateShroundState(disableShround);
+            }
+            else
+            {
+                disableShround = false;
+                UpdateShroundState(disableShround);
+            }
+        }
+
+        private void UpdateShroundState(bool disableShround)
+        {
+            this.self.Owner.Shroud.Disabled = disableShround;
+            if (self.World.LocalPlayer == self.Owner)
+                self.World.RenderPlayer = disableShround ? null : self.Owner;
+        }
+    }
+}
