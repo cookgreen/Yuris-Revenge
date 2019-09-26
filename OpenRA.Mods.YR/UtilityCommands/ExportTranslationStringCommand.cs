0ac63507039463d676c6b0e6f3782bbae1775a92
Cook Green <dell20091234@gmail.com>
2019-09-26 11:53:05 +0800

    New utility command which can export translation string and add sample translation file

---
+++ OpenRA.Mods.YR/UtilityCommands/ExportTranslationStringCommand.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenRA.Mods.YR.UtilityCommands
+{
+	public class ExportTranslationStringCommand : IUtilityCommand
+	{
+		public string Name { get { return "--export-translation-string"; } }
+		bool IUtilityCommand.ValidateArguments(string[] args) { return args.Length >= 2; }
+
+		[Desc("LOCALIZATIONNAME", "Export strings in rules, sequences and chrome into a yaml file with the localization name")]
+		public void Run(Utility utility, string[] args)
+		{
+			var modData = utility.ModData;
+
+			var localizationName = args[1];
+
+			var localizationFile = localizationName + ".yaml";
+			if(File.Exists(localizationFile))
+			{
+				File.Delete(localizationFile);
+			}
+			using (StreamWriter writer = new StreamWriter(File.Create(localizationName + ".yaml")))
+			{
+				List<MiniYamlNode> subNodes = new List<MiniYamlNode>();
+				List<MiniYamlNode> ruleNodes = new List<MiniYamlNode>();
+				foreach (var f in modData.Manifest.Rules)
+				{
+					var actors = MiniYaml.FromStream(modData.DefaultFileSystem.Open(f), f);
+
+					// TODO: maybe can export actorInfos
+					foreach (var actor in actors)
+					{
+						foreach (var trait in actor.Value.Nodes)
+						{
+							// TODO: export the string which has translation identifier
+							if (trait.Key == "Tooltip")
+							{
+								ruleNodes.Add(new MiniYamlNode(actor.Key, new MiniYaml(trait.Value.Nodes[0].Value.Value)));
+							}
+						}
+					}
+				}
+				subNodes.Add(new MiniYamlNode("Rules", new MiniYaml(null, ruleNodes)));
+
+				List<MiniYamlNode> nodes = new List<MiniYamlNode>();
+				string upperFirst = localizationName.Substring(0, 1).ToUpper();
+				var newLocalizationName = localizationName.Replace(localizationName.Substring(0, 1), upperFirst);
+				MiniYamlNode node = new MiniYamlNode(localizationName, new MiniYaml(newLocalizationName, subNodes));
+				nodes.Add(node);
+				
+				MiniYaml tranlsation = new MiniYaml(null, nodes);
+				foreach (var line in tranlsation.ToLines(localizationName))
+				{
+					writer.WriteLine(line);
+				}
+			}
+		}
+	}
+}
