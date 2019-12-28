f03aad14cd02699ae1a5a47418c95b08f1bd1b88
Cook Green <dell20091234@gmail.com>
2019-12-28 09:29:31 +0800

    Display Current Content Path and Fix #97

---
+++ OpenRA.Mods.YR/UtilityCommands/DisplayCurrentContentPathCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenRA.Mods.YR.UtilityCommands
+{
+    class DisplayCurrentContentPath : IUtilityCommand
+    {
+        public string Name { get { return "--display-content-path"; } }
+
+        bool IUtilityCommand.ValidateArguments(string[] args) { return true; }
+
+        [Desc("", "Display current openra content path")]
+        public void Run(Utility utility, string[] args)
+        {
+            Console.WriteLine("OpenRA Content Path: " + Platform.ResolvePath("^Content"));
+        }
+    }
+}
