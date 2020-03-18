3ec748cec41790975f29a859700c9fbca017146a
Cook Green <dell20091234@gmail.com>
2020-03-18 13:38:50 +0800

    Add Dummy Pcx Loader

---
+++ OpenRA.Mods.YR/FileFormats/Pcx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenRA.Mods.YR.FileFormats
+{
+	public class Pcx
+	{
+		public Pcx(Stream s)
+		{
+
+		}
+	}
+}
---
+++ OpenRA.Mods.YR/SpriteLoaders/PcxLoader.cs
@@ -0,0 +1,31 @@
+﻿using OpenRA;
+using OpenRA.Graphics;
+using OpenRA.Primitives;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenRA.Mods.YR.SpriteLoaders
+{
+	public class PcxLoader : ISpriteLoader
+	{
+		class PcxFrame : ISpriteFrame
+		{
+			public Size Size { get; set; }
+			public Size FrameSize { get; set; }
+			public float2 Offset { get; set; }
+			public byte[] Data { get; set; }
+			public bool DisableExportPadding { get { return false; } }
+		}
+		public bool TryParseSprite(Stream s, out ISpriteFrame[] frames, out TypeDictionary metadata)
+		{
+			frames = new ISpriteFrame[1];
+			metadata = new TypeDictionary();
+
+			return true;
+		}
+	}
+}
