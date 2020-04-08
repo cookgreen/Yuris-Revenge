6eb0dd52f1ce87b986e1f6e3cad80c796e15abf7
Cook Green <dell20091234@gmail.com>
2020-04-08 13:05:01 +0800

    Update Project

--- OpenRA.Mods.YR/Activities/MarkerSlaveLeave.cs
+++ OpenRA.Mods.YR/Activities/MarkerSlaveLeave.cs
@@ -33,7 +33,7 @@ namespace OpenRA.Mods.YR.Activities
 
             self.World.AddFrameEndTask(w =>
             {
-                if (!master.IsDead)
+                if (!master.IsDead && master.IsInWorld)
                 {
                     master.Trait<MarkerMaster>().PickupSlave(master, slave);
                 }
