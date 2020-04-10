b3835c83c90ef5e6fde71ce511b17be3838ffa78
Cook Green <dell20091234@gmail.com>
2020-04-10 15:12:40 +0800

    WDT

---
+++ OpenRA.Mods.YR/Widgets/Logic/AddWDTScenarioSuffixLogic.cs
@@ -0,0 +1,14 @@
+﻿using OpenRA.Widgets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenRA.Mods.YR.Widgets.Logic
+{
+	public class AddWDTScenarioSuffixLogic : ChromeLogic
+	{
+
+	}
+}
